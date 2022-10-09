e6ff624497c9ac90cab79388a468dca0b9e6d9b5
tkdgns179 <tkdgns179@knou.ac.kr>
2022-10-09 22:35:45 +0900

    설정 파일 분리 수정, User 엔티티 테이블명 변경,

--- src/main/java/com/example/orderapp/domain/User.java
+++ src/main/java/com/example/orderapp/domain/User.java
@@ -4,35 +4,46 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 @Entity
 @NoArgsConstructor
-public class User {
+@Table(name = "ORDER_USER")
+public class User extends BaseTimeEntity {
 
     @Id
     @Column(name = "phone_number")
     private String phoneNumber;
 
-    @Column(name = "user_name", length = 10)
+    @Column(name = "user_name", length = 10, nullable = false)
     private String userName;
 
-    @Column(name = "user_nickname", length = 20)
+    @Column(name = "user_nickname", length = 20, nullable = false)
     private String userNickname;
 
-    @Column(name = "user_password", length = 20)
+    @Column(name = "user_password", length = 20, nullable = false)
     private String userPassword;
 
     //TODO 주문번호 FK키 number - order_id
 
-    @Column(name = "user_address", length = 50)
+    @Column(name = "user_address", length = 50, nullable = false)
     private String userAddress;
 
     @Enumerated
-    @Column(name = "user_type")
-    private String userType;
+    @Column(name = "user_type", nullable = false)
+    private UserType userType;
 
-    @Column(name = "create_date")
-    private LocalDate
+    @Getter
+    @AllArgsConstructor
+    enum UserType {
+        Customer("손님"),
+        Store("점포");
 
+        private String typeName;
+    }
 }
+
